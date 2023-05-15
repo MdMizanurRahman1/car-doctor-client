d57724e8b3a27ded3ded8a2c25b91106f03f22a0
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2023-05-16 01:48:32 +0300

    delete method and tabular form done

--- src/pages/Bookings/Bookings.jsx
+++ src/pages/Bookings/Bookings.jsx
@@ -1,21 +1,71 @@
 import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../providers/AuthProvider';
+import BookingRow from './BookingRow';
 
 const Bookings = () => {
 
     const { user } = useContext(AuthContext);
     const [bookings, setBookings] = useState([]);
-    const uri = `http://localhost:5000/bookings?email=${user.email}`;
+    const uri = `http://localhost:5000/bookings?email=${user?.email}`;
 
     useEffect(() => {
         fetch(uri)
             .then(res => res.json())
-            .then(data => console.log(data))
+            .then(data => setBookings(data))
     }, [])
+
+    const handleDelete = id => {
+        const proceed = confirm('Are you sure you want to delete');
+        if (proceed) {
+            fetch(`http://localhost:5000/bookings/${id}`, {
+                method: 'DELETE'
+            })
+                .then(res => res.json())
+                .then(data => {
+                    console.log(data)
+                    if (data.deletedCount > 0) {
+                        alert('Deleted successfully')
+                        const remaining = bookings.filter(booking => booking._id !== id)
+                        setBookings(remaining)
+                    }
+                })
+        }
+    }
+
 
     return (
         <div>
+            <h2 className='text-3xl'>My Bookings: {bookings.length}</h2>
+            <div className="overflow-x-auto w-full">
+                <table className="table w-full">
+                    {/* head */}
+                    <thead>
+                        <tr>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <th>Image</th>
+                            <th>Service</th>
+                            <th>Date</th>
+                            <th>Price</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
 
+                        {
+                            bookings.map(booking => <BookingRow
+                                key={booking._id}
+                                booking={booking}
+                                handleDelete={handleDelete}
+                            ></BookingRow>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
