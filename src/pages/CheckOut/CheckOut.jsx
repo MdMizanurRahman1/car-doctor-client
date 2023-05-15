9c95354df73b5fdf5ea020132f071b547d427d85
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2023-05-15 22:47:03 +0300

    post method done

---
+++ src/pages/CheckOut/CheckOut.jsx
@@ -0,0 +1,88 @@
+import React, { useContext } from 'react';
+import { useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../providers/AuthProvider';
+import Swal from 'sweetalert2'
+
+const CheckOut = () => {
+    const service = useLoaderData();
+    const { _id, title, price, img } = service;
+    const { user } = useContext(AuthContext);
+
+    const handleBookService = event => {
+        event.preventDefault()
+        const form = event.target;
+        const name = form.name.value;
+        const email = user?.email;
+        const date = form.date.value;
+        const amount = form.amount.value;
+
+        const booking = {
+            customerName: name,
+            email,
+            img,
+            date,
+            amount,
+            service: title,
+            service_id: _id,
+            price: price
+
+        }
+        fetch('http://localhost:5000/bookings', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(booking)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.insertedId) {
+                    Swal.fire('Booking added successfully!')
+                }
+            })
+
+    }
+
+    return (
+        <div>
+            <h2 className='text-center text-3xl'>Book services: {title} </h2>
+
+            <form onSubmit={handleBookService}>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" defaultValue={user?.name} name='name' className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Date</span>
+                        </label>
+                        <input type="date" name='date' className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="text" defaultValue={user?.email} name='email' placeholder="email" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Due amount</span>
+                        </label>
+                        <input type="text" name='amount' defaultValue={'$' + price} className="input input-bordered" />
+                    </div>
+                </div>
+                <div className="form-control mt-6">
+                    <input type="submit" value="Order Confirm" className="btn btn-block btn-error" />
+                </div>
+
+            </form>
+        </div>
+    );
+};
+
+export default CheckOut;
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
 import { createContext, useEffect, useState } from "react";
-import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from "../../public/firebase/firebase.config";
 
 
@@ -20,10 +20,14 @@ const AuthProvider = ({ children }) => {
         return signInWithEmailAndPassword(auth, email, password);
     }
 
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
-            console.log('current user', currentUser);
             setLoading(false);
         });
         return () => {
@@ -35,7 +39,8 @@ const AuthProvider = ({ children }) => {
         user,
         loading,
         createUser,
-        signIn
+        signIn,
+        logOut
     }
 
     return (
