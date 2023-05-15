963eb5558db36e36d7109fc75d657da1e4dd5eb3
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2023-05-16 00:13:20 +0300

    some data loaded

--- src/pages/CheckOut/CheckOut.jsx
+++ src/pages/CheckOut/CheckOut.jsx
@@ -36,7 +36,7 @@ const CheckOut = () => {
         })
             .then(res => res.json())
             .then(data => {
-                console.log(data)
+
                 if (data.insertedId) {
                     Swal.fire('Booking added successfully!')
                 }
